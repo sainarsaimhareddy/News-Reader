c1afd7e761d632df80ed58d66684a2be819af590
sainarasimhareddy <reddyyynarasimha4@gmail.com>
2025-04-18 15:02:12 +0530

    BUILT BASIC FETCHING AND NAVBAR COMPONENTS

---
+++ newsreader/src/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+// import { Link } from 'react-router-dom'
+export default function Navbar() {
+    return (
+<nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <a className="navbar-brand" href="#">News Today</a>
+  <button c="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+    );
+}
